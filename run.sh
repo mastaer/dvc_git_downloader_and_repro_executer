2f19b28e972ffd75e57d9a97faba66d26ead335d
Jonas <jonas.annuscheit@gmail.com>
2019-03-07 13:30:23 +0100

    change run

--- run.sh
+++ run.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-git clone $1
+echo "VERSION 0.3"
 
 #
-# $1 repository i.e.: 
+# $1 repository i.e.: git.tools.f4.htw-berlin.de
 # $2 own username
 # $3 own password
 # $4 username of owner of repo
 # $5 reponame
 # $6 username dvc
-# $7 server and path to dvc
+# $7 server to dvc
 # $8 password of ssh access to dvc
 # $9 git branchname to checkout
-#
+# $10 path to dvc server
 
 
 
 
 
 # clone repository
-git clone https://$2:$3@$1/$4/$5
+git clone https://$2:$3@$1/$4/$5/
 
 a=$?
 if [ $a -gt 0 ]
@@ -29,6 +29,7 @@ then
 fi
 
 
+echo $(ls)
 
 
 # go in folder of repository
@@ -41,9 +42,10 @@ then
     exit $a
 fi
 
+echo $(ls)
 
-
-echo "\n['remote \"nas\"']\nurl = ssh://$6@$7\npassword = '$8'\n\n[core]\nremote = nas" > .dvc/config.local
+echo "\n['remote \"nas\"']\nurl = ssh://$6@$7${10}\npassword = '$8'\n\n[core]\nremote = nas" > .dvc/config.local
+#echo "\n['remote \"nas\"']\nurl = ssh://$6@$7\npassword = '$8'\ndir = "${10}"\n\n[core]\nremote = nas" > .dvc/config.local
 
 a=$?
 if [ $a -gt 0 ]
@@ -53,7 +55,7 @@ then
 fi
 
 
-
+echo $(ls)
 
 git checkout $9
 
@@ -65,7 +67,7 @@ then
 fi
 
 
-
+echo $(ls)
 
 dvc pull
 
